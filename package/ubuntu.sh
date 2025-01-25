a25c8062e2a2bc3c6f852f8bd5f026228acb3cf9
Vadim Kalinnikov <moose@ylsoftware.com>
2025-01-25 19:35:33 +0300

    Fixed build on Ubuntu 24.10

--- package/ubuntu.sh
+++ package/ubuntu.sh
@@ -2,26 +2,39 @@
 
 ROOTPATH=$(dirname $(dirname $(readlink -f $0)))
 
-IMGNAME=qopdsbrowser-noble-build
 
 PKG_VER="$1"
 PPA_VER="$2"
 
+DISTR_LIST="noble oracular"
+DISTR_LIST="oracular"
 
-if [ -z $PPA_VER ]; then
-    echo "Usage:"
-    echo "  $0 VERSION BUILD"
-    echo " "
-    echo "Example:"
-    echo "  $0 0.0.6 1"
-    exit
-fi
+buildpkg() {
+	PKG_VER="$1"
+	PPA_VER="$2"
+	PKG_DISTR="$3"
+
+	IMGNAME=qopdsbrowser-${PKG_DISTR}-build
+
+	if [ -z $PPA_VER ]; then
+	    echo "Usage:"
+	    echo "  $0 VERSION BUILD"
+	    echo " "
+	    echo "Example:"
+	    echo "  $0 0.0.6 1"
+	    exit
+	fi
 
 
-docker build -f package/ubuntu/Dockerfile -t $IMGNAME $ROOTPATH
+	docker build -f package/ubuntu/Dockerfile.${PKG_DISTR} -t $IMGNAME $ROOTPATH
 
-docker run --rm -it \
-    -e PKG_VERSION=${PKG_VER} \
-    -e PKG_PPAVER=${PPA_VER} \
-    -v $ROOTPATH:/source \
-    -t $IMGNAME
+	docker run --rm -it \
+	    -e PKG_VERSION=${PKG_VER} \
+	    -e PKG_PPAVER=${PPA_VER} \
+	    -v $ROOTPATH:/source \
+	    -t $IMGNAME
+}
+
+for distr in $DISTR_LIST; do
+	buildpkg $PKG_VER $PPA_VER $distr
+done
